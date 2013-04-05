25801f183136a63870fb03d13804c6530d9453e8
Dave Kroondyk <dave.kroondyk@thomsonreuters.com>
2013-04-05 12:51:25 -0400

    use __send__ instead of [] to access object attributes.
    This is for when you might want to access regular instance variables
    instead of ActiveModel attributes

--- lib/audited/auditor.rb
+++ lib/audited/auditor.rb
@@ -176,7 +176,7 @@ module Audited
 
       def audited_changes
         changed_attributes.except(*non_audited_columns).inject({}) do |changes,(attr, old_value)|
-          changes[attr] = [old_value, self[attr]]
+          changes[attr] = [old_value, self.__send__(attr.to_sym)]
           changes
         end
       end
